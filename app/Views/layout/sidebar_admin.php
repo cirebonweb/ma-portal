2f5d3838b9baff940a280fa5370cddf3642571ff
Mahmud <machmudseo@gmail.com>
2026-01-18 19:12:22 +0700

    rev: grup menu advertising

--- app/Views/layout/sidebar_admin.php
+++ app/Views/layout/sidebar_admin.php
@@ -1,5 +1,5 @@
-<li class="nav-header">BILLBOARD</li>
-<?= $this->include('layout/sidebar_admin_billboard') ?>
+<li class="nav-header">ADVERTISING</li>
+<?= $this->include('layout/sidebar_admin_advertising') ?>
 
 <li class="nav-header">VENDOR</li>
 <?= $this->include('layout/sidebar_vendor') ?>
